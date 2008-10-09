dd98e7ac6826858fbcc85dc3a5788d4a28e5f035
Mike West <mike@mikewest.org>
2008-10-09 22:18:21 +0200

    This doesn't work yet.  In the process of stealing ideas from Sinatra.

--- fallow.rb
+++ fallow.rb
@@ -8,6 +8,7 @@ module Fallow
   ROOT_DIR  = File.expand_path(File.dirname(__FILE__))
   DATA_DIR  = ROOT_DIR + '/data'
   
+  
   autoload :Dispatch,   'fallow/dispatch'
   
   autoload :Article,    'fallow/article'
@@ -17,4 +18,36 @@ module Fallow
   autoload :Template,   'fallow/template'
 
   autoload :ErrorPage,  'fallow/error'
+
+  def request_for(path, &block)
+    URI_COMPONENTS = {
+      :year   => '\d{4}',
+      :month  => '\d{2}',
+      :slug   => '[0-9A-Za-z-_]+'
+    }
+    path.each { |path|
+      URI.encode(path)
+    }
+  end
+
+#
+# Potential Page Types (vaguely like Sinatra)
+#
+  request_for ['/'] {
+    # Homepage
+  }
+  
+  request_for ['/:year/:month/:slug/?'] {
+    # Article
+  }
+  
+  request_for ['/archive/','/archive/:year/?'] {
+    # Archive
+  }
+  
+#
+# Potential Error States
+#
+  class NotFound < Exception
+  end
 end
